99056d23066a17bd52334610ba29962fef3ac9a5
kanamiyasato <90434830+kanamiyasato@users.noreply.github.com>
2022-10-12 12:28:49 -0400

    Create name.rb

---
+++ variables/name.rb
@@ -0,0 +1,6 @@
+# Program that asks the user to type in their name 
+# and then prints out a greeting message with their name included
+
+print "What's your name? "
+name = gets.chomp
+puts "Hello #{name}!"
