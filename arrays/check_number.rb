b177853b6b20644225baf91f163a6697d408e945
Kana <miyasatoka@gmail.com>
2022-10-15 20:32:16 -0400

    Update check_number program

--- arrays/check_number.rb
+++ arrays/check_number.rb
@@ -2,3 +2,13 @@
 
 arr = [1, 3, 5, 7, 9, 11]
 number = 3
+
+# Program 1
+answer = arr.include?(number)
+puts "Does #{number} appear in the array?"
+puts answer
+
+# Program 2
+if arr.include?(number)
+  puts "#{number} is indeed in the array."
+end
